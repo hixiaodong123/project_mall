8c92fd5a7b8ff0118a295d2c9a5587b1d309cb2b
hixiaodong123 <420350696@qq.com>
2019-08-16 15:05:48 +0800

    完成了分页插件的配置

--- src/main/java/com/cskaoyan/mall/bean/Goods.java
+++ src/main/java/com/cskaoyan/mall/bean/Goods.java
@@ -3,7 +3,8 @@ package com.cskaoyan.mall.bean;
 import java.math.BigDecimal;
 import java.util.Date;
 
-public class Goods {
+public class Goods
+{
     private Integer id;
 
     private String goodsSn;
@@ -46,171 +47,243 @@ public class Goods {
 
     private String detail;
 
-    public Integer getId() {
+    public Integer getId()
+    {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Integer id)
+    {
         this.id = id;
     }
 
-    public String getGoodsSn() {
+    public String getGoodsSn()
+    {
         return goodsSn;
     }
 
-    public void setGoodsSn(String goodsSn) {
+    public void setGoodsSn(String goodsSn)
+    {
         this.goodsSn = goodsSn == null ? null : goodsSn.trim();
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name == null ? null : name.trim();
     }
 
-    public Integer getCategoryId() {
+    public Integer getCategoryId()
+    {
         return categoryId;
     }
 
-    public void setCategoryId(Integer categoryId) {
+    public void setCategoryId(Integer categoryId)
+    {
         this.categoryId = categoryId;
     }
 
-    public Integer getBrandId() {
+    public Integer getBrandId()
+    {
         return brandId;
     }
 
-    public void setBrandId(Integer brandId) {
+    public void setBrandId(Integer brandId)
+    {
         this.brandId = brandId;
     }
 
-    public String getGallery() {
+    public String getGallery()
+    {
         return gallery;
     }
 
-    public void setGallery(String gallery) {
+    public void setGallery(String gallery)
+    {
         this.gallery = gallery == null ? null : gallery.trim();
     }
 
-    public String getKeywords() {
+    public String getKeywords()
+    {
         return keywords;
     }
 
-    public void setKeywords(String keywords) {
+    public void setKeywords(String keywords)
+    {
         this.keywords = keywords == null ? null : keywords.trim();
     }
 
-    public String getBrief() {
+    public String getBrief()
+    {
         return brief;
     }
 
-    public void setBrief(String brief) {
+    public void setBrief(String brief)
+    {
         this.brief = brief == null ? null : brief.trim();
     }
 
-    public Boolean getIsOnSale() {
+    public Boolean getIsOnSale()
+    {
         return isOnSale;
     }
 
-    public void setIsOnSale(Boolean isOnSale) {
+    public void setIsOnSale(Boolean isOnSale)
+    {
         this.isOnSale = isOnSale;
     }
 
-    public Short getSortOrder() {
+    public Short getSortOrder()
+    {
         return sortOrder;
     }
 
-    public void setSortOrder(Short sortOrder) {
+    public void setSortOrder(Short sortOrder)
+    {
         this.sortOrder = sortOrder;
     }
 
-    public String getPicUrl() {
+    public String getPicUrl()
+    {
         return picUrl;
     }
 
-    public void setPicUrl(String picUrl) {
+    public void setPicUrl(String picUrl)
+    {
         this.picUrl = picUrl == null ? null : picUrl.trim();
     }
 
-    public String getShareUrl() {
+    public String getShareUrl()
+    {
         return shareUrl;
     }
 
-    public void setShareUrl(String shareUrl) {
+    public void setShareUrl(String shareUrl)
+    {
         this.shareUrl = shareUrl == null ? null : shareUrl.trim();
     }
 
-    public Boolean getIsNew() {
+    public Boolean getIsNew()
+    {
         return isNew;
     }
 
-    public void setIsNew(Boolean isNew) {
+    public void setIsNew(Boolean isNew)
+    {
         this.isNew = isNew;
     }
 
-    public Boolean getIsHot() {
+    public Boolean getIsHot()
+    {
         return isHot;
     }
 
-    public void setIsHot(Boolean isHot) {
+    public void setIsHot(Boolean isHot)
+    {
         this.isHot = isHot;
     }
 
-    public String getUnit() {
+    public String getUnit()
+    {
         return unit;
     }
 
-    public void setUnit(String unit) {
+    public void setUnit(String unit)
+    {
         this.unit = unit == null ? null : unit.trim();
     }
 
-    public BigDecimal getCounterPrice() {
+    public BigDecimal getCounterPrice()
+    {
         return counterPrice;
     }
 
-    public void setCounterPrice(BigDecimal counterPrice) {
+    public void setCounterPrice(BigDecimal counterPrice)
+    {
         this.counterPrice = counterPrice;
     }
 
-    public BigDecimal getRetailPrice() {
+    public BigDecimal getRetailPrice()
+    {
         return retailPrice;
     }
 
-    public void setRetailPrice(BigDecimal retailPrice) {
+    public void setRetailPrice(BigDecimal retailPrice)
+    {
         this.retailPrice = retailPrice;
     }
 
-    public Date getAddTime() {
+    public Date getAddTime()
+    {
         return addTime;
     }
 
-    public void setAddTime(Date addTime) {
+    public void setAddTime(Date addTime)
+    {
         this.addTime = addTime;
     }
 
-    public Date getUpdateTime() {
+    public Date getUpdateTime()
+    {
         return updateTime;
     }
 
-    public void setUpdateTime(Date updateTime) {
+    public void setUpdateTime(Date updateTime)
+    {
         this.updateTime = updateTime;
     }
 
-    public Boolean getDeleted() {
+    public Boolean getDeleted()
+    {
         return deleted;
     }
 
-    public void setDeleted(Boolean deleted) {
+    public void setDeleted(Boolean deleted)
+    {
         this.deleted = deleted;
     }
 
-    public String getDetail() {
+    public String getDetail()
+    {
         return detail;
     }
 
-    public void setDetail(String detail) {
+    public void setDetail(String detail)
+    {
         this.detail = detail == null ? null : detail.trim();
+    }
+
+
+    @Override
+    public String toString()
+    {
+        final StringBuffer sb = new StringBuffer("Goods{");
+        sb.append("id=").append(id);
+        sb.append(", goodsSn='").append(goodsSn).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", categoryId=").append(categoryId);
+        sb.append(", brandId=").append(brandId);
+        sb.append(", gallery='").append(gallery).append('\'');
+        sb.append(", keywords='").append(keywords).append('\'');
+        sb.append(", brief='").append(brief).append('\'');
+        sb.append(", isOnSale=").append(isOnSale);
+        sb.append(", sortOrder=").append(sortOrder);
+        sb.append(", picUrl='").append(picUrl).append('\'');
+        sb.append(", shareUrl='").append(shareUrl).append('\'');
+        sb.append(", isNew=").append(isNew);
+        sb.append(", isHot=").append(isHot);
+        sb.append(", unit='").append(unit).append('\'');
+        sb.append(", counterPrice=").append(counterPrice);
+        sb.append(", retailPrice=").append(retailPrice);
+        sb.append(", addTime=").append(addTime);
+        sb.append(", updateTime=").append(updateTime);
+        sb.append(", deleted=").append(deleted);
+        sb.append(", detail='").append(detail).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 }
--- src/test/java/com/cskaoyan/mall/test/MyTest.java
+++ src/test/java/com/cskaoyan/mall/test/MyTest.java
@@ -1,11 +1,16 @@
 package com.cskaoyan.mall.test;
 
+import com.cskaoyan.mall.bean.Goods;
 import com.cskaoyan.mall.mapper.GoodsMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 /**
  * @description: 测试类
@@ -23,7 +28,17 @@ public class MyTest
     @Test
     public void name()
     {
-        long l = goodsMapper.countByExample(null);
-        System.out.println(l);
+        //查询所有的订单,封装成List
+        PageHelper.startPage(Integer.parseInt("1"), Integer.parseInt("10"));
+        List<Goods> list = goodsMapper.selectByExample(null);
+        PageInfo<Goods> pageInfo = new PageInfo<>(list);
+        long total = pageInfo.getTotal();
+
+        for (Goods goods : list)
+        {
+            System.out.println(goods);
+        }
+        System.out.println(total);
+
     }
 }
