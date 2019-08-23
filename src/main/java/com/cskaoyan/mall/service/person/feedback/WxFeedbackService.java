2cea790a2d1084703e8b4f72e9a09fee0d800a7c
hixiaodong123 <420350696@qq.com>
2019-08-23 17:14:03 +0800

    用户反馈完成

---
+++ src/main/java/com/cskaoyan/mall/service/person/feedback/WxFeedbackService.java
@@ -0,0 +1,15 @@
+package com.cskaoyan.mall.service.person.feedback;
+
+import com.cskaoyan.mall.bean.Feedback;
+
+/**
+ * @description: 用户反馈业务接口
+ * @author: Lime
+ * @create: 2019-08-23 16:57
+ **/
+
+public interface WxFeedbackService
+{
+
+    void insert(Feedback feedback);
+}
